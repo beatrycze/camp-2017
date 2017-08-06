d0cde786535e6023cc37d44f307db5dbcdb873ab
Beata Szczygielska <beata.szczygielska@gmail.com>
2017-08-06 23:12:54 +0200

    Validation on login input

--- js/login.js
+++ js/login.js
@@ -1,13 +1,25 @@
 (() => {
 
     const login = document.getElementById('userLogin');
+    const password = document.getElementById('userPassword');
     const button = document.getElementsByClassName('button')[0];
+    const errorText = document.querySelector('p.hidden');
 
-    // console.log(login, button);
+    // console.log(login, password, button);
 
     button.addEventListener('click', event => {
+        if(login.value) {
+            login.classList.remove('error');            
+            errorText.classList.add('hidden');
+            window.setTimeout(function(){
+                window.location = "./index.html";
+            }, 500);
+            console.log( login.value );
+        } else {
+            login.classList.add('error');            
+            errorText.classList.remove('hidden');
+        }
         event.preventDefault();
-        console.log( login.value );
     });
 
 })();

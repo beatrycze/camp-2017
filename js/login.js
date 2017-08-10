5a528e293869c6149915310007f58916d70f974f
Beata Szczygielska <beata.szczygielska@gmail.com>
2017-08-10 21:09:38 +0200

    Tooltip on form validation added

--- js/login.js
+++ js/login.js
@@ -6,47 +6,71 @@
     const errorLogin = document.querySelector('p.login');
     const errorPassword = document.querySelector('p.password');
 
+    const validationMessage = document.querySelector('div.validation-message');
+
     // console.log(login, password, button);
 
-    const acceptLogin = () => {
-        login.classList.remove('error');
-        errorLogin.classList.add('hidden');
-    }
+    // const acceptLogin = () => {
+    //     login.classList.remove('error');
+    //     errorLogin.classList.add('hidden');
+    // }
 
-    const displayLoginError = () => {
-        login.classList.add('error');
-        errorLogin.classList.remove('hidden');
-    }
+    // const displayLoginError = () => {
+    //     login.classList.add('error');
+    //     errorLogin.classList.remove('hidden');
+    // }
 
-    const acceptPassword = () => {
-        password.classList.remove('error');
-        errorPassword.classList.add('hidden');
-    }
+    // const acceptPassword = () => {
+    //     password.classList.remove('error');
+    //     errorPassword.classList.add('hidden');
+    // }
 
-    const displayPasswordError = () => {
-        password.classList.add('error');
-        errorPassword.classList.remove('hidden');
-    }
+    // const displayPasswordError = () => {
+    //     password.classList.add('error');
+    //     errorPassword.classList.remove('hidden');
+    // }
+
+    // button.addEventListener('click', event => {
+    //     if (!login.value && password.value) { // login empty
+    //         acceptPassword();
+    //         displayLoginError();
+    //     } else if (login.value && !password.value) { // password empty
+    //         acceptLogin();
+    //         displayPasswordError();
+    //     } else if (!login.value && !password.value) { // login & password empty
+    //         displayLoginError();
+    //         displayPasswordError();
+    //     } else { // login & password not empty
+    //         acceptLogin();
+    //         acceptPassword();
+    //         window.setTimeout(function() {
+    //             window.location = "./index.html";
+    //         }, 500);
+    //         // console.log(login.value, password.value);
+    //     }
+    //     event.preventDefault();
+    // });
 
     button.addEventListener('click', event => {
-        if (!login.value && password.value) { // login empty
-            acceptPassword();
-            displayLoginError();
-        } else if (login.value && !password.value) { // password empty
-            acceptLogin();
-            displayPasswordError();
-        } else if (!login.value && !password.value) { // login & password empty
-            displayLoginError();
-            displayPasswordError();
-        } else { // login & password not empty
-            acceptLogin();
-            acceptPassword();
-            window.setTimeout(function() {
-                window.location = "./index.html";
-            }, 500);
-            // console.log(login.value, password.value);
-        }
         event.preventDefault();
+        $.ajax({
+            type: "post",
+            data: {
+                login: login.value,
+                password: password.value
+            },
+            url: "https://efigence-camp.herokuapp.com/api/login",
+            error: function(response) {
+                // console.log(response);
+                login.classList.add('error');
+                password.classList.add('error');
+                validationMessage.classList.remove('hidden');
+                validationMessage.innerHTML=response.responseJSON.message;
+            },
+            success: function(response) {
+                window.location = "./index.html";
+            }
+        });
     });
 
 })();

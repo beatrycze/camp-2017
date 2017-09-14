08b972d9f97e9517a1daa5e687bcfb1c8ef7f5e4
Beata Szczygielska <beata.szczygielska@gmail.com>
2017-09-15 00:31:11 +0200

    Form validation refactored

--- js/transfer-form.js
+++ js/transfer-form.js
@@ -1,160 +1,133 @@
 (() => {
 
-    // $('form[data-validate]').each((formIndex, formElement) => {
-    //     $(formElement).find('input[data-validate], select[data-validate]')
-    //     .each((inputIndex, inputElement) => {
-    //         let validators = $(inputElement).data('validate').split(' ');
-    //         console.log(validators);
-    //     })
-    // });
-    // // http://subinsb.com/jquery-to-javascript#find
+    const notEmpty = (value) => value.trim().length > 0;
+    const properAccountFormat = (value) => value.replace(/\s/g,'').match(/^[0-9]{26}$/) != null;
+    const properAmountFormat = (value) => value.trim().match(/^\d*(.|,)?\d\d?$/) !=null && value.trim().match(/[1-9]{1}/) !=null;
 
+    const properDateFormat = (value) => value.match(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/) !=null;
+    const dateNotFromPast = (input) => {
+        let dateValid = properDateFormat(input.value);
+        let selectedDate = input.value.replace(/\/|\./g,'-').split('-').reverse().join('-');
+        let currentDate = new Date();
+        let currentDateToCompare = [ currentDate.getFullYear(), ("0" + (currentDate.getMonth() + 1)).slice(-2), currentDate.getDate() ].join('-');
+        if ( dateValid && ( selectedDate < currentDateToCompare) ) {
+            // console.log('Wpisz datę bieżącą lub przyszłą.');
+        } else if ( dateValid && (selectedDate >= currentDateToCompare) ) {
+            return true;
+        }
+    };
+
+    const rbChecked = (input) => input.checked;
+
+    const customFieldClassName = 'custom-border';
     const fieldErrorClassName = 'input-select-error';
 
-    const fieldNotEmpty = (field) => field.value.trim().length > 0;
-    const properAccountFormat = (input) => input.value.replace(/\s/g,'').match(/^[0-9]{26}$/) != null;
-    const properAmountFormat = (input) => input.value.trim().match(/^\d*(.|,)?\d\d?$/) !=null && input.value.trim().match(/[1-9]{1}/) !=null;
+    const stringToValidatorMap = {
+        notEmpty: notEmpty,
+        accountFormat: properAccountFormat,
+        amountFormat: properAmountFormat,
+        dateFormat: dateNotFromPast,
+        rbChecked: rbChecked
+    };
 
-    const validate = (valid, selector, className) => {
+    let fieldValidationRules = [];
+    $('form[data-validate]').each((formIndex, formElement) => {
+    // why jQuery .find(): http://subinsb.com/jquery-to-javascript#find
+        $(formElement).find('input[data-validate][type="text"], select[data-validate]')
+        .each((fieldIndex, fieldElement) => {
+            let validators = $(fieldElement).data('validate').split(' ');
+            let targetErrorSelectors = ( fieldElement.parentNode.classList.contains(customFieldClassName) ) ? [fieldElement.parentNode] : [fieldElement];
+            fieldValidationRules.push({
+                targetErrorSelectors: targetErrorSelectors,
+                element: fieldElement,
+                validators: validators.map(validator => stringToValidatorMap[validator])
+            });
+        });
+
+        let rbsAggregate = $(formElement).find('input[type="radio"]').toArray().reduce((aggr, node) => {
+            if(!(node.name in aggr)) {
+                aggr[node.name] = [];
+            }
+            aggr[node.name].push(node);
+            return aggr;
+        }, {});
+        for(let rbName in rbsAggregate) {
+            if(rbsAggregate.hasOwnProperty(rbName)) {
+                let radioButtons = rbsAggregate[rbName];
+                let validators = $(radioButtons[0]).data('validate').split(' ');
+                let targetErrorSelectors = radioButtons.map(rb => rb.parentNode);
+                fieldValidationRules.push({
+                    targetErrorSelectors: targetErrorSelectors,
+                    elements: radioButtons,
+                    validators: validators.map(validator => stringToValidatorMap[validator])
+                });
+            }
+        }
+    });
+
+    function fieldIsCorrect(value, validators) {
+        return validators.reduce(function(aggr, validator) {
+            return aggr && validator(value);
+        }, true);
+    }
+
+    function atLeastOneTruthy(booleans) {
+        return booleans.reduce(function(aggr, bool) {
+            return aggr || bool;
+        }, false);
+    }
+
+    const displayErrorCss = (valid, selectors, className) => {
         if (!valid) {
-            selector.classList.add(className);
+            selectors.forEach(selector => selector.classList.add(className));
         } else {
-            selector.classList.remove(className);
+            selectors.forEach(selector => selector.classList.remove(className));
         }
     };
 
-    const validateRadioButtons = (selector, rbList, className) => {
-        if(!selector) {
-            rbList.forEach(element => element.classList.add(className));
-        } else {
-            rbList.forEach(element => element.classList.remove(className));
-        }
-    };
-
-    // const accountNativeSelect = document.getElementById('account-select');
-    // const accountCustomSelect = document.querySelector('div.account-custom-select');
-    // function validateTransferAccount() {
-    //     let valid = fieldNotEmpty(accountNativeSelect);
-    //     validate(valid, accountCustomSelect, 'custom-select-error');
-    // }
-    // accountNativeSelect.addEventListener('blur', validateTransferAccount);
-
-    // const recipientInput = document.getElementById('recipient');
-    // const recipientInputContainer = document.querySelector('div.recipient-input-container');
-    // function validateTransferRecipient() {
-    //     let valid = fieldNotEmpty(recipientInput);
-    //     validate(valid, recipientInputContainer, fieldErrorClassName);
-    // }
-    // recipient.addEventListener('blur', validateTransferRecipient);
-
-    const numberInput = document.getElementById('number');
-    function validateTransferNumber() {
-        let valid = properAccountFormat(numberInput);
-        validate(valid, numberInput, fieldErrorClassName);
-    }
-    numberInput.addEventListener('blur', validateTransferNumber);
-
-    const sumInput = document.getElementById('sum');
-    function validateTransferSum() {
-        let valid = properAmountFormat(sumInput);
-        validate(valid, sumInput, fieldErrorClassName);
-    }
-    sumInput.addEventListener('blur', validateTransferSum);
-
-    // const currencySelect = document.getElementById('currency-select');
-    // function validateTransferCurrency() {
-    //     // let valid = currencySelect.value.length > 0; // mogę to spr także za pomocą fieldNotEmpty
-    //     let valid = fieldNotEmpty(currencySelect);
-    //     validate(valid, currencySelect, fieldErrorClassName);
-    // }
-    // currencySelect.addEventListener('blur', validateTransferCurrency);
-
-    // TODO define date format
-    // const whenInput = document.getElementById('when');
-    // const whenContainer = document.querySelector('div.when-input-container');
-    // function validateTransferDate() {
-    //     let valid = fieldNotEmpty(whenInput);
-    //     validate(valid, whenContainer, fieldErrorClassName);
-    // }
-    // whenInput.addEventListener('blur', validateTransferDate);
-
-    // const titleInput = document.getElementById('title');
-    // function validateTransferTitle() {
-    //     let valid = fieldNotEmpty(titleInput);
-    //     validate(valid, titleInput, fieldErrorClassName);
-    // }
-    // titleInput.addEventListener('blur', validateTransferTitle);
-
-    function generateFieldContentValidator(fieldSelector, fieldContainer) {
-        if (fieldContainer) {
-            function checkIfNotEmpty() {
-                let valid = fieldNotEmpty(fieldSelector);
-                validate(valid, fieldContainer, fieldErrorClassName);
+    function validateOnSubmit(fieldValidationRules) {
+        fieldValidationRules.forEach(function(field) {
+            if(field.element) {
+                let isCorrect = fieldIsCorrect(field.element.value, field.validators);
+                if (!isCorrect) {
+                    // console.log(`Wiedz, że coś się dzieje z ${field.fieldType}`);
+                }
+                displayErrorCss(isCorrect, field.targetErrorSelectors, fieldErrorClassName);
+            } else if (field.elements) {
+                let isCorrect = atLeastOneTruthy(field.elements.map(rbChecked));
+                displayErrorCss(isCorrect, field.targetErrorSelectors, fieldErrorClassName);
             }
-            fieldSelector.addEventListener('blur', checkIfNotEmpty);
-        } else {
-            function checkIfNotEmpty() {
-                let valid = fieldNotEmpty(fieldSelector);
-                validate(valid, fieldSelector, fieldErrorClassName);
+        });
+    }
+
+    function validateOnBlur(fieldValidationRules) {
+        fieldValidationRules.forEach(function(field) {
+            if(field.element) {
+                field.element.addEventListener('blur', function() {
+                    let isCorrect = fieldIsCorrect(field.element.value, field.validators);
+                    displayErrorCss(isCorrect, field.targetErrorSelectors, fieldErrorClassName);
+                });
             }
-            fieldSelector.addEventListener('blur', checkIfNotEmpty);
-        }
-        return checkIfNotEmpty;
+        });
     }
 
-    const accountNativeSelect = document.getElementById('account-select');
-    const accountCustomSelect = document.querySelector('div.account-custom-select');
-    const validateTransferAccount = generateFieldContentValidator(accountNativeSelect, accountCustomSelect);
-
-    const recipientInput = document.getElementById('recipient');
-    const recipientInputContainer = document.querySelector('div.recipient-input-container');
-    const validateTransferRecipient = generateFieldContentValidator(recipientInput, recipientInputContainer);
-
-    // const validateTransferSumNotEmpty = generateFieldContentValidator(sumInput);
-
-    const currencySelect = document.getElementById('currency-select');
-    const validateTransferCurrency = generateFieldContentValidator(currencySelect);
-
-    // TODO define date format
-    const whenInput = document.getElementById('when');
-    const whenContainer = document.querySelector('div.when-input-container');
-    const validateTransferDate = generateFieldContentValidator(whenInput, whenContainer);
-
-    const titleInput = document.getElementById('title');
-    const validateTransferTitle = generateFieldContentValidator(titleInput);
-
-    function generateRbValidator(rbSelector, rbName) {
-        const rbWrappersList = document.querySelectorAll(rbSelector);
-        function validateRbs() {
-            const rbSelected = document.querySelector(`input[name=${rbName}]:checked`);
-            validateRadioButtons(rbSelected, rbWrappersList, 'rb-error');
-        }
-        const rbList = document.querySelectorAll(`input[name=${rbName}]`);
-        rbList.forEach(element => element.addEventListener('click', validateRbs));
-        return validateRbs;
+    function validateRbsOnClick(fieldValidationRules) {
+        fieldValidationRules.forEach(function(field) {
+            if(field.elements) {
+                field.elements.forEach(element => element.addEventListener('click', function() {
+                    let isCorrect = rbChecked(element);
+                    displayErrorCss(isCorrect, field.targetErrorSelectors, fieldErrorClassName);
+                }));
+            }
+        });
     }
 
-    const validateTransferType = generateRbValidator('div.type-icon-bg', 'transfer-type');
-    const validateTransferWhere = generateRbValidator('div.where-rb-wrapper', 'transfer-where');
-    const validateTransferOption = generateRbValidator('div.option-rb-wrapper', 'transfer-options');
-
-    const validateTransferForm = function() {
-        validateTransferAccount();        
-        validateTransferType();
-        validateTransferRecipient();
-        validateTransferWhere();
-        validateTransferNumber();
-        // validateTransferSumNotEmpty();
-        validateTransferSum();
-        validateTransferCurrency();
-        validateTransferDate();
-        validateTransferTitle();
-        validateTransferOption();
-    };
+    validateOnBlur(fieldValidationRules);
+    validateRbsOnClick(fieldValidationRules);
 
     const transferForm = document.getElementById('transfer');
     transferForm.addEventListener('submit', function(event) {
         event.preventDefault();
-        validateTransferForm();
+        validateOnSubmit(fieldValidationRules);
     });
 })();
